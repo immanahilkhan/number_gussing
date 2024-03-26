87952d73172afc9d9b5a0dac7cf387ddc31b0546
Manahil Khalid <hussainmanhilkhalid@gmail.com>
2024-03-26 20:08:13 +0500

    creted number gussing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node is ko bolta hai shabang 
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "guessNumber",
+        type: "number",
+        message: "guess the number 1 - 6"
+    }
+]);
+if (answer.guessNumber === randomNumber) {
+    console.log("congrats you guess right number ");
+}
+else {
+    console.log("sorry you'r not guess right number ");
+    console.log("game over ");
+}
